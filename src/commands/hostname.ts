9c8c7d32c9ea6051c20a9a9bc119242fd851c9f3
Hugo Josefson <hugo@josefson.org>
2022-02-17 21:02:06 +0000

    feat: proxmox hostname

--- src/commands/hostname.ts
+++ src/commands/hostname.ts
@@ -4,6 +4,7 @@ import { FileSystemPath } from "../model/dependency.ts";
 import { debian3SystemInstallation } from "./debian-3-system-installation.ts";
 import { Command } from "../model/command.ts";
 import { ROOT } from "../os/user/root.ts";
+import { netmask } from "../deps.ts";
 
 export const etcHostname = new CreateFile(
   ROOT,
@@ -18,10 +19,17 @@ function getHostnames() {
     .join(" ");
 }
 
+function getIp(): string {
+  if (config.IP === "dhcp") {
+    return "127.0.1.1";
+  }
+  return netmask(config.IP).ip;
+}
+
 export const etcHosts = new LineInFile(
   ROOT,
   FileSystemPath.of(ROOT, "/mnt/etc/hosts"),
-  `127.0.1.1       ${getHostnames()}`,
+  `${getIp()}       ${getHostnames()}`,
 )
   .withDependencies([debian3SystemInstallation]);
 
