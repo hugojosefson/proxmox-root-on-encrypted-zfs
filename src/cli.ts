cc260bc8fc52b7d9622a933c8842ea1791c604bc
Hugo Josefson <hugo@josefson.org>
2022-02-17 22:17:30 +0000

    feat: remove sudo-keepalive
    We don't need it when booting from the Debian live ISO.

--- src/cli.ts
+++ src/cli.ts
@@ -8,8 +8,6 @@ import { colorlog } from "./deps.ts";
 import { Command, CommandResult } from "./model/command.ts";
 import { RejectFn } from "./os/defer.ts";
 import { isRunningAsRoot } from "./os/user/is-running-as-root.ts";
-import { sudoKeepalive } from "./os/user/sudo-keepalive.ts";
-import { targetUserPromise } from "./os/user/target-user.ts";
 import { run } from "./run.ts";
 import { errorAndExit, usageAndExit } from "./usage.ts";
 
@@ -28,51 +26,46 @@ export const cli = async () => {
 
   const commands: Command[] = await Promise.all(args.map(getCommand));
   const runCommandsPromise = run(commands);
-  const stopSudoKeepalive: () => void = sudoKeepalive(await targetUserPromise);
-  try {
-    await runCommandsPromise.then(
-      (results: Array<CommandResult>) => {
-        results.forEach((result) => {
-          if (result.stdout) console.error(colorlog.success(result.stdout));
-          if (result.stderr) console.error(colorlog.error(result.stderr));
-          if (!(result?.status?.success)) {
-            console.error(JSON.stringify(result.status));
-          }
-        });
-        const anyError: CommandResult | undefined = results.find((result) =>
-          (!result.status.success) ||
-          (result.status.code > 0)
-        );
-        if (anyError) {
-          const err: CommandResult = anyError;
-          Deno.exit(err.status.code);
+  await runCommandsPromise.then(
+    (results: Array<CommandResult>) => {
+      results.forEach((result) => {
+        if (result.stdout) console.error(colorlog.success(result.stdout));
+        if (result.stderr) console.error(colorlog.error(result.stderr));
+        if (!(result?.status?.success)) {
+          console.error(JSON.stringify(result.status));
         }
-      },
-      // deno-lint-ignore no-explicit-any : because Promise defines it as ?any
-      (err?: any): RejectFn => {
-        if (config.VERBOSE) {
-          if (err?.message) {
-            console.error("err.message: " + colorlog.error(err.message));
-          }
-          if (err?.stack) {
-            console.error("err.stack: " + colorlog.warning(err.stack));
-          }
-          if (err?.stdout) {
-            console.error("err.stdout: " + colorlog.success(err.stdout));
-          }
-          if (err?.stderr) {
-            console.error("err.stderr: " + colorlog.error(err.stderr));
-          }
+      });
+      const anyError: CommandResult | undefined = results.find((result) =>
+        (!result.status.success) ||
+        (result.status.code > 0)
+      );
+      if (anyError) {
+        const err: CommandResult = anyError;
+        Deno.exit(err.status.code);
+      }
+    },
+    // deno-lint-ignore no-explicit-any : because Promise defines it as ?any
+    (err?: any): RejectFn => {
+      if (config.VERBOSE) {
+        if (err?.message) {
+          console.error("err.message: " + colorlog.error(err.message));
+        }
+        if (err?.stack) {
+          console.error("err.stack: " + colorlog.warning(err.stack));
+        }
+        if (err?.stdout) {
+          console.error("err.stdout: " + colorlog.success(err.stdout));
+        }
+        if (err?.stderr) {
+          console.error("err.stderr: " + colorlog.error(err.stderr));
+        }
 
-          console.error("err: " + colorlog.error(JSON.stringify(err, null, 2)));
-        }
-        const code: number = err?.status?.code || err?.code || 1;
-        Deno.exit(code);
-      },
-    );
-  } finally {
-    stopSudoKeepalive();
-  }
+        console.error("err: " + colorlog.error(JSON.stringify(err, null, 2)));
+      }
+      const code: number = err?.status?.code || err?.code || 1;
+      Deno.exit(code);
+    },
+  );
 };
 
 if (import.meta.main) {
