99c4a7b8b8d3a42389847d4a0f1df78179668662
Hugo Josefson <hugo@josefson.org>
2024-10-30 17:05:33 +0100

    local: delete create-vdisks.sh

--- create-vdisks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-IFS=$'\t\n'
-
-sudo qemu-img create -f qcow2 /var/lib/libvirt/images/debian11.qcow2 128G
-sudo qemu-img create -f qcow2 /var/lib/libvirt/images/debian11-1.qcow2 500G
-sudo qemu-img create -f qcow2 /var/lib/libvirt/images/debian11-2.qcow2 1024G
-
