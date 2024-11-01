50049d25c789258c556e029833183672eb9dacf0
Hugo Josefson <hugo@josefson.org>
2024-11-01 17:32:20 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fix: handle roots better

--- encrypt-zpool.sh
+++ encrypt-zpool.sh
@@ -5,7 +5,7 @@
 # and properties.
 #
 # Usage:
-# wget -O encrypt-zpool.sh https://raw.githubusercontent.com/hugojosefson/proxmox-root-on-encrypted-zfs/3d60087/encrypt-zpool.sh && chmod +x encrypt-zpool.sh && bash -x ./encrypt-zpool.sh
+# wget -O encrypt-zpool.sh https://raw.githubusercontent.com/hugojosefson/proxmox-root-on-encrypted-zfs/0be1226/encrypt-zpool.sh && chmod +x encrypt-zpool.sh && bash -x ./encrypt-zpool.sh 2>&1 | less
 #
 # Prerequisites:
 #   - Proxmox VE 8 installation ISO
@@ -432,7 +432,7 @@ encrypt_dataset_or_load_key() {
       fi
 
       ___ "Take snapshot of ${dataset}"
-      zfs snapshot -R "${snapshot}"
+      zfs snapshot -r "${snapshot}"
 
       ___ "Encrypt ${dataset}"
       echo "Encrypting dataset: ${dataset}"
